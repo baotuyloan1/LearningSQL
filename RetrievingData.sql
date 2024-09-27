6bcc12a96be8a88da977d150caea41e2216d25dd
Nguyen Duc Bao <nguyenduc.bao@avenue-net.com>
2024-09-27 16:42:43 +0700

    SELECTING and SORTING

---
+++ RetrievingData.sql
@@ -0,0 +1,165 @@
+-- Retrieving Invidual Columns
+SELECT prod_name
+FROM Products;
+
+-- The output from this statement is shown in the following
+/*
+	prod_name
+--------------------
+	Fish bean bag toy
+	Bird bean bag toy
+	Rabbit bean bag toy
+	8 inch teddy bear
+	12 inch teddy bear
+	18 inch teddy bear
+	Raggedy Ann
+	King doll
+	Queen doll
+*/
+
+/*
+	NOTE: It is important to note that SQL statements are not case sensitive, so SELECT is the same as select.
+
+	All extra white space within a SQLstatement is ignored when that statement is processed. SQLstatements can be specified on one long line or broken up over many lines. So, the following three statements are functionally identical:
+*/
+SELECT prod_name
+FROM Products;
+
+SELECT prod_name FROM Products;
+
+SELECT
+prod_name
+FROM
+Products; 
+
+-- A reserverd word that is a part of the SQL language. Never name a table or column using a keyword.
+/*
+	To use SELECT to retrieve table data, you must, at a minimin, specify two pieces of information - what you want to select and from where you want to select it
+*/
+
+
+-- Retrieving Multiple Columns
+/*
+To retrieve multiple columns from a table, the same SELECT statement is used. The
+only difference is that multiple column names must be specified after the SELECT
+keyword, and each column must be separated by a comma.
+
+Tip: Take care with commas
+When selecting multiple columns, be sure to specify a comma between each
+column name, but not after the last column name. Doing so will generate an
+error.
+*/
+SELECT prod_id, prod_name, prod_price FROM Products;
+
+/*
+Just as in the prior example, this statement uses the SELECT statement to retrieve
+data from the Products table. In this example, three column names are specified,
+each separated by a comma. The output from this statement is shown below:
+
+prod_id prod_name prod_price
+--------- -------------------- ----------
+BNBG01 Fish bean bag toy 3.49
+BNBG02 Bird bean bag toy 3.49
+BNBG03 Rabbit bean bag toy 3.49
+BR01 8 inch teddy bear 5.99
+BR02 12 inch teddy bear 8.99
+BR03 18 inch teddy bear 11.99
+RGAN01 Raggedy Ann 4.99
+RYL01 King doll 9.49
+RYL02 Queen dool 9.49
+*/
+
+/*
+NOTE: Presentation of Data
+SQLstatements typically return raw, unformatted data, and different DBMSs
+and clients may display the data differently. Data formatting is a presentation issue, not a
+retrieval issue. Therefore, presentation is typically specified in the
+application that displays the data.
+*/
+
+/**/
+
+/*
+	Retrieving all columns
+	using the asterisk (*) wildcard character
+*/
+SELECT *
+FROM Products;
+
+/*
+	As a rule, you are better of not using the * wildcard unless you really do
+need every column in the table. Even though use of wildcards may save you
+the time. Even though use of wildcards may save you
+the time and effort needed to list the desired columns explicitly,retrieving
+unnecessary columns usually slows down the performance of your retrieval
+and your application.
+*/
+
+/*
+Tip: Retrieving Unknown Columns
+
+There is one big advantage to using wildcards. As you do not explicitly
+specify column names (because the asterisk retrieves every column), it is
+possible to retrieve columns whose names are unknown.
+*/
+
+-- =============== Retrieving Distinct Rows================
+SELECT DISTINCT vend_id FROM Products;
+
+/*
+SELECT DISTINCT vend_id tells the DBMS to only return distinct (unique) vend_id rows, and so only three rows are returned.
+If used, the DISTINCT keyword must be placed directly in front of the column names
+*/
+
+/*
+Caution: Can't be Partially DISTINCT
+The DISTINCT keyword applies to all columns, not just the one it precedes. If
+you were to specify SELECT DISTINCT vend_id, prod_price, six of the nine
+rows would be retrieved because the combined specified columns produced
+six unique combinations.
+*/
+SELECT DISTINCT vend_id, prod_price FROM Products;
+SELECT vend_id, prod_price FROM Products;
+
+--====================Limiting Results====================
+SELECT  TOP 2   prod_name FROM Products;
+
+/*
+Output:
+prod_name
+-----------------
+8 inch teddy bear
+12 inch teddy bear
+18 inch teddy bear
+Fish bean bag toy
+Bird bean bag toy
+*/
+
+/*
+The previous statement uses the SELECT TOP 5 statement to retrieve just the first
+five rows.
+*/
+
+
+/*
+To get the next five rows, specify both where to start and the number of rows to
+retrieve, like this:
+
+*/
+
+SELECT prod_name
+FROM ProductsORDER BY prod_name -- required, when getting the next five rows,OFFSET 5 ROWS -- numbers row ignoreFETCH NEXT 5 ROWS ONLY;  -- limit results/*ORDER BY prod_nameOFFSET 5 ROWSFETCH NEXT 5 ROWS ONLYprod_name
+-------------------King doll                                                                                                                                                                                                                                                      
+Queen doll                                                                                                                                                                                                                                                     
+Rabbit bean bag toy                                                                                                                                                                                                                                            
+Raggedy Ann                                                                                                                                                                                                                                                    ORDER BY is required,  OFFETS 5 ROWS FETCH NEXT 5 ROWS ONLY instructs supported DBMSs return fieve rows starting from row 5.The first number is where to start, and the second number is the number of rows to retrieve.So FETCH NEXT specifies the number of rows to return,  OFFSET ... ROWS specifies where to start from. In the example, there are only nine products in the Products table so  OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY returned just four row (as there was no fifth)*//*Caution: Row 0The first row retrieved is is row 0, not row 1. Such as, ORDER BY prod_name OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY; will retrieve the second row, not the first one.*//*Challengens1. Write a SQLstatement to retrieve all customer IDs (cust_id) from the
+Customers table.
+
+2. The OrderItems table contains every item ordered (and some were ordered multiple times). Write a SQLstatement to retrieve a list of the products
+(prod_id) ordered (not every order, just a unique list of products). Here’s a
+hint: you should end up with seven unique rows displayed.
+
+3. Write a SQLstatement that retrieves all columns from the Customers table
+and an alternate SELECT that retrieves just the customer ID. Use comments to
+comment out one SELECT so as to be able to run the other. (And, of course,
+test both statements.)*/-- 1SELECT cust_id FROM Customers;-- 2SELECT DISTINCT prod_id FROM OrderItems;-- 3SELECT * FROM Customers;SELECT cust_id FROM Customers;
---
+++ SortingRetrievedData.sql
@@ -0,0 +1,236 @@
+SELECT prod_name
+FROM Products;/*The data appears to be
+displayed in no particular order at all.prod_name
+--------------------
+Fish bean bag toy
+Bird bean bag toy
+Rabbit bean bag toy
+8 inch teddy bear
+12 inch teddy bear
+18 inch teddy bear
+Raggedy Ann
+King doll
+Queen doll*//*if data was subsequently updated or deleted, the order will be affected
+by how the DBMS reuses reclaimed storage space*/
+
+/*
+To explicitly sort data retrieved using a SELECT statement, you use the ORDER BY
+clause.
+ORDER BY takes the name of one or more columns by which to sort the
+output.
+*/
+
+SELECT prod_name
+FROM Products
+ORDER BY prod_name;
+
+/*
+prod_name
+--------------------
+12 inch teddy bear
+18 inch teddy bear
+8 inch teddy bear
+Bird bean bag toy
+Fish bean bag toy
+King doll
+Queen doll
+Rabbit bean bag toy
+Raggedy Ann
+*/
+
+/*
+Caution: Position of ORDER BY clause
+
+When specifying an ORDER BY clause, be sure that it is the last clause in your
+SELECT statement. If it is not the last clause, an error will be generated.
+*/
+
+/*
+Although more often than not the columns used in an ORDER BY clause will be
+ones selected for display, this is actually not required. It is perfectly legal to
+sort data by a column that is not retrieved.
+*/
+
+SELECT vend_id
+FROM Products
+ORDER BY prod_name;
+
+
+-- ============Sorting by Multiple Columns===============
+/*
+To sort by multiple columns, simply specify the column names separated by
+commas (just as you do when you are selecting multiple columns).
+*/
+
+/*
+
+e, if you aredisplaying an employee list, you might want to display it sorted by last name and
+first name (first by last name, and then within each last name sort by first name). This type of sort would be useful if there are multiple employees with the same
+last name
+
+The following code retrieves three columns and sorts the results by two of them —first by price and then by name.
+*/
+
+SELECT prod_id, prod_price, prod_name
+FROM Products
+ORDER BY prod_price, prod_name;/*prod_id prod_price prod_name
+------- ---------- --------------------
+BNBG02 3.4900 Bird bean bag toy
+BNBG01 3.4900 Fish bean bag toy
+BNBG03 3.4900 Rabbit bean bag toy
+RGAN01 4.9900 Raggedy Ann
+BR01 5.9900 8 inch teddy bear
+BR02 8.9900 12 inch teddy bear
+RYL01 9.4900 King doll
+RYL02 9.4900 Queen doll
+BR03 11.9900 18 inch teddy bearIt is important to understand that when you are sorting by multiple columns, the
+sort sequence is exactly as specified. In other words, using the output in the
+example above, the products are sorted by the prod_name column only when multiple rows have the same prod_price value. If all the values in the prod_pricecolumn had been unique, no data would have been sorted by prod_name.*/
+-- ============================ Sorting by Column Position=======================================
+/*
+, ORDER BY
+also supports ordering specified by relative column position
+*/
+SELECT prod_id, prod_price, prod_name
+FROM Products
+ORDER BY 2, 3;
+
+/*
+prod_id prod_price prod_name
+------- ---------- --------------------
+BNBG02 3.4900 Bird bean bag toy
+BNBG01 3.4900 Fish bean bag toy
+BNBG03 3.4900 Rabbit bean bag toy
+RGAN01 4.9900 Raggedy Ann
+BR01 5.9900 8 inch teddy bear
+BR02 8.9900 12 inch teddy bear
+RYL01 9.4900 King doll
+RYL02 9.4900 Queen doll
+BR03 11.9900 18 inch teddy bear
+*/
+
+/*
+As you can see, the output is identical to that of the query above. The difference
+here is in the ORDER BY clause. Instead of specifying column names, you specify
+the relative positions of selected columns in the SELECT list. ORDER BY 2 means
+sort by the second column in the SELECT list, the prod_price column. ORDER BY 2,
+3 means sort by prod_price and then by prod_name
+*/
+
+/*
+
+The primary advantage of this technique is that it saves retyping the column
+names. But there are some downsides too. First, not explicitly listing column
+names increases the likelihood of you mistakenly specifying the wrong column.
+Second, it is all too easy to mistakenly reorder data when making changes to the
+SELECT list (forgetting to make the corresponding changes to the ORDER BY clause). And finally, obviously you cannot use this technique when sorting by columns that
+are not in the SELECT list.
+*/
+
+
+--=========================================== Specifying Sort Direction ==========================================--
+/*
+The following example sorts the products by price in descending order (most
+expensive first):
+*/
+
+SELECT prod_id, prod_price, prod_name
+FROM Products
+ORDER BY prod_price DESC;
+
+/*
+prod_id prod_price prod_name
+------- ---------- --------------------
+BR03 11.9900 18 inch teddy bear
+RYL01 9.4900 King doll
+RYL02 9.4900 Queen doll
+BR02 8.9900 12 inch teddy bear
+BR01 5.9900 8 inch teddy bear
+RGAN01 4.9900 Raggedy Ann
+BNBG01 3.4900 Fish bean bag toy
+BNBG02 3.4900 Bird bean bag toy
+BNBG03 3.4900 Rabbit bean bag toy
+*/
+
+/*
+The following example sorts
+the products in descending order (most expensive first), plus product name:
+*/
+
+SELECT prod_id, prod_price, prod_name FROM Products ORDER BY prod_price DESC, prod_name;
+
+/*
+
+prod_id prod_price prod_name
+------- ---------- --------------------
+BR03 11.9900 18 inch teddy bear
+RYL01 9.4900 King doll
+RYL02 9.4900 Queen doll
+BR02 8.9900 12 inch teddy bear
+BR01 5.9900 8 inch teddy bear
+RGAN01 4.9900 Raggedy Ann
+BNBG02 3.4900 Bird bean bag toy
+BNBG01 3.4900 Fish bean bag toy
+BNBG03 3.4900 Rabbit bean bag toy
+*/
+
+/*
+The DESC keyword only applies to the column name that directly precedes it. In
+the example above, DESC was specified for the prod_price column, but not for
+the prod_name column. Therefore, the prod_price column is sorted in descending
+order, but the prod_name column (within each price) is still sorted in standard
+ascending order.
+*/
+
+
+/*
+Caution: Sorting Descending on Multiple ColumnsIf you want to sort descending on multiple columns, be sure each column has
+its own DESC keyword.
+*/
+
+/*
+Tip: Case Sensitivity and Sort OrdersWhen you are sorting textual data, is A the same as a? And does a come before B or after Z? These are not theoretical questions, and the answers depend on how the database is set up
+
+In dictionary sort order, A is treated the same as a, and that is the default
+behavior for most DBMSs. However, most good DBMSs enable database
+administrators to change this behavior if needed. (If your database contains
+lots of foreign language characters, this might become necessary.)
+The key here is that, if you do need an alternate sort order, you may not be
+able to accomplish this with a simple ORDER BY clause. You may need to
+contact your database administrator.
+
+*/
+
+/*
+ORDER BY clasue must be the last in the SELECT statement, can
+be used to sort data on one or more columns as needed.
+*/
+
+/*
+Challenges
+1. Write a SQLstatement to retrieve all customer names (cust_names) from the
+Customers table, and display the results sorted from Z to A.
+
+2. Write a SQLstatement to retrieve customer ID (cust_id) and order number
+(order_num) from the Orders table, and sort the results first by customer ID
+and then by order date in reverse chronological order.
+
+3. Our fictitious store obviously prefers to sell more expensive items, and lots
+of them. Write a SQLstatement to display the quantity and price
+(item_price) from the OrderItems table, sorted with the highest quantity and
+highest price first.
+
+4. What is wrong with the following SQLstatement? (Try to figure it out without running it):
+SELECT vend_name,
+FROM Vendors
+ORDER vend_name DESC;
+- There should not be a comma after vender_name( a comma is only used to separate multiple columns), and BY is missing after ORDER
+*/
+
+
+SELECT cust_name FROM Customers ORDER BY cust_name DESC;
+
+SELECT cust_id, order_num FROM Orders ORDER BY cust_id, order_date DESC;
+
+SELECT quantity, item_price FROM OrderItems ORDER BY quantity DESC, item_price DESC;
+
