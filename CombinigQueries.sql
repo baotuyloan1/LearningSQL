946deccd839c687ee975be975f0a0222136c0bf4
Nguyen Duc Bao <nguyenduc.bao@avenue-net.com>
2024-10-02 17:56:08 +0700

    Combining Queries, Creating Advanced Joins

---
+++ CombinigQueries.sql
@@ -0,0 +1,304 @@
+-- ============== Combining Queries ======== --
+
+-- ==== Understanding Combined Queries === --
+/*
+Most SQL queries contain a single SELECT statement that returns data from one or 
+more tables. SQL also enables you to perform multiple queries (multiple SELECT 
+statements) and return the results as a single query result set. These combined
+queries are usually known as unions or compound queries.
+
+There are basically two scanarios in which you'd use combined queries:
+	- To return similarly structured data from different tables in a single query.
+	- To perform multiple queries against a single table returning the data as one query.
+*/
+
+/*
+Tip: Combining Queries and Multiple WHERE conditions
+
+For the most part, combining two queries to the same table accomplishes the same thing as a single query
+with multiple WHERE clause condions. In other words, any SELECT statement with multiple WHERE clauses can 
+also be specified as a combined query.
+*/
+
+/*
+Creating Combined Queries
+SQL queries are combined using the UNION operator. Using UNION, you can specifu multiple SELECT statements,
+and their results can be combined into a single result set.
+*/
+
+-- == Using UNION == --
+/*
+Using UNION is simple enough. All you do is specify each SELECT statement 
+and place the keyword UNION between each.
+
+Example: You need a report on all your customers in Illinois, 
+Indiana, and Michiga. You also want to include all Fun4All locations, 
+regarless of state. Of course, you can create a WHERE clase that will do this, but this time
+use a UNION instead.
+
+Creating a UNION involves writing multiple SELECT statements.
+*/
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI');
+
+/*
+cust_name		cust_contact		cust_email
+-----------		-------------		------------
+Village Toys	John Smith			sales@villagetoys.com
+Fun4All			Jim Jones			jjones@fun4all.com
+The Toy Store	Kim Howard			NULL
+*/
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All';
+
+/*
+cust_name		cust_contact		cust_email
+------------	-----------			-------------
+Fun4All			Jim Jones			jjones@fun4all.com
+Fun4All			Denise L. Stephens	dstephens@fun4all.com
+*/
+
+/*
+The first SELECT retrieves all rows in Illinois, Indiana, and Michigan by passing
+those state abbreviations to IN clause. The second SELECT uses a simple equality 
+test to find all Fun4All locations. You'll notice that one row appears on both outputs
+as it meets both WHERE conditions.
+To combine these two statements, do the following:
+*/
+
+SELECT cust_name as custName1, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI')
+UNION
+SELECT cust_name AS custName2 , cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All1'
+UNION
+SELECT cust_name AS custName3 , cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All'
+ORDER BY custName1 DESC;
+
+
+/*
+cust_name		cust_contact		cust_email
+-----------		-----------			----------------
+Fun4All			Denise L. Stephens	dstephens@fun4all.com
+Fun4All			Jim Jones			jjones@fun4all.com
+Village Toys	John Smith			sales@villagetoys.com
+The Toy Store	Kim Howard			NULL
+*/
+
+/*
+The preceding statements are made up of both of the previous SELECT statements separated by the
+UNION keyword. UNION instructs the DBMS to execute both SELECT statements and combine the output
+into a single query result set.
+
+As a point of reference, the same query using multiple WHERE clases instead of a UNION:
+*/
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI') 
+	OR cust_name = 'Fun4All';
+
+/*
+In our simple example, the UNION might actually be more complicated than using a WHERE clause.
+But with more complex filtering conditions, or if the data is being retrieved from multiple tables
+(and not just a single table), the UNION could have made the process much simpler indeed.
+*/
+
+/*
+Tip: UNION Limits
+There is no standard SQL limit to the number of SELECT statements that can be combined with
+UNION statements. However, it is best to consult your DBMS documentation to ensure that it does not enforce
+any maximum statement restrictions of its own.
+*/
+
+/*
+Caution: Performance Issues
+Most good DBMSs use an internal query optimizer to combine the SELECT statements before they are even processed.
+In theory, this means that from a performance perspective, there should be not real difference between using
+multiple WHERE clause conditions or a UNION. I say in "theory", because, in practice, most query optimizers
+don't always do as good a job as they should. Your best bet is to test both methods to see which will work best
+for you
+*/
+
+-- == UNION Rules === --- 
+/*
+As you can see, unions are very easy to use. But there are a few rules governing exactly which can be combined:
+	- A UNION must be composed of two or more SELECT statements, each 
+	separated by the keyword UNION(so, if you're combining four SELECT statements, you would use three UNION keywords)
+	- Each query in a UNION must contain the same columns, expression, or aggregate function (and some DBMSs even require that columns 
+	be listed in the same order).
+	- Column datatypes must be compatible. They need not be the same name or the exact same type, but
+	they must be of a type that the DBMS can implicitly convert (for example, different numeric types of different data types)
+*/
+
+/*
+Note: UNION Column Names
+If SELECT statements that are combined with a UNION have diferent column names, what name is actually returned? 
+For example, if one statement contained SELECT produc_name and the next used SELECT productname, what would be the the name 
+of the combined returned column?
+
+The answer is that the first name is used, so in our example the combined column would be named prod_name, even though the second 
+SELECT used a different name. This is also means that you can use an alias on the first name to set the returned column name as needed.
+
+This behavior has another interesting side effect. Because the first set of 
+column names are used, only those names can be specified when shorting.
+Again, in our example, you could use ORDER BY prod_name to sort the
+combined results, but ORDER BY productname would display an error message because
+there is no column productname in the combined results.
+*/
+
+/*
+Aside from these basic rules and restrictions, unions can be used for any data retrieval tasks.
+*/
+
+-- ===================== Including or Eliminating Duplicate Rows =================== --
+/*
+Go back to the preceding section titled "Using UNION" and look at the sample
+SELECT statements used. You'll notice that when executed individually, the first SELECT
+statement returns three rows, and the second SELECT statement returns two rows. 
+However, when the two SELECT statements are combined with a UNION, only four rows are returned, not five.
+
+The UNION automatically removes any duplicate rows from the query result set (in other words, it behaves just as
+multiple WHERE clase conditions in a single SELECT would). Because there is a Fun4All location in Indian, that row
+was returned by both SELECT statements. WHhen the UNION was used, the duplicate row was eliminated.
+
+This is the default behavior of UNION, but you can change it if you so desire. If you
+would, in fact,want all occurences of all matches returned, you could use UNION ALL instead of UNION.
+*/
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI')
+UNION ALL
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All';
+
+/*
+cust_name			cust_contact		cust_email
+-----------			-------------		------------
+Village Toys		John Smith s		ales@villagetoys.com
+Fun4All				Jim Jones			jjones@fun4all.com
+The Toy Store		Kim Howard			NULL
+Fun4All				Jim Jones			jjones@fun4all.com
+Fun4All				Denise L. Stephens	dstephens@fun4all.com
+*/
+
+/*
+When you use UNION ALL, the DBMS does not eliminate duplicates. Therefore, the preceding example
+returns five rows, one of them occurring twice.
+*/
+
+/*
+Tip: UNION versus WHERE
+
+UNION almost always accomplishes the same thing as multiple WHERE conditions.
+UNION ALL is the form of UNION that accomplishes what cannot be done with WHERE clauses.
+If you do, in fact, wall all occurrences of matches for every condition (including duplicates), you must use UNION ALL and no WHERE.
+*/
+
+-- =========== Sorting COmbined Query Results ============= --
+/*
+SELECT statement output is sorted using the ORDER BY clause. When combining 
+queries with a UNION, you may use only one ORDER BY clause, and it must occur after the final SELECT statement.
+There is very little point in sorting part of a result set one way and part another way, and so multiple ORDER BY 
+clauses are not allowed.
+
+The following example sorts the results returned by the previously used UNION:
+*/
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN ('IL','IN','MI')
+UNION
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All'
+ORDER BY cust_name, cust_contact;
+
+/*
+cust_name			cust_contact		cust_email
+-----------			-------------		-------------
+Fun4All				Denise L. Stephens	dstephens@fun4all.com
+Fun4All				Jim Jones			jjones@fun4all.com
+The Toy Store		Kim Howard			NULL
+Village Toys		John Smith s		ales@villagetoys.com
+*/
+
+/*
+This UNION takes a single ORDER BY clause after the final SELECT statement. Even 
+though the ORDER BY appears to be a part of only that last SELECT statement, the
+DBMS will in fact use it to sort all the results returned by all the SELECT statements.
+*/
+
+/*
+Note: Other UNION Types
+Some DBMSs support two additional types of UNION. EXCCEPT(sometimes called MINUS) can be used to retrieve only
+the rows that exist in the first table but not in the second, and INTERSECT can be used to retrieve only the rows
+that exist in both tables. In practice, however, these UNION types are rarely used because the same results can
+be accomplished using joins.
+*/
+
+/*
+Tip: Working with Multiple Tables
+For simplicity's sake, the previous examples have all used UNION to combine multiple queries
+on the same table. In practice, UNION is really useful, when you need to combine data from multiple tables,
+even tables with mismatched column names, in which case you can combine UNION with aliases to retrieve a
+single set of results
+*/
+
+/*
+Challenges
+
+1. Write a SQLstatement that combines two SELECT statements that retrieve
+product ID (prod_id) and quantity from the OrderItems table, one filtering
+for rows with a quantity of exactly 100, and the other filtering for products with an ID that begins with BNBG. Sort the results by product ID.
+*/
+SELECT prod_id, quantity
+FROM OrderItems
+WHERE quantity = 100
+UNION 
+SELECT prod_id, quantity
+FROM OrderItems
+WHERE prod_id LIKE 'BNBG%'
+ORDER BY prod_id;
+
+/*
+2. Rewrite the SQLstatement you just created to use a single SELECT statement.
+*/
+SELECT prod_id, quantity
+FROM OrderItems
+WHERE quantity = 100 OR prod_id LIKE 'BNBG%'
+ORDER BY prod_id;
+/*
+
+3. This one is a little nonsensical, I know, but it does reinforce a note earlier inthis lesson. Write a SQLstatement which returns and combines product name(prod_name) from Products and customer name (cust_name) from Customers,
+and sort the result by product name.
+*/
+SELECT prod_name
+FROM Products
+UNION
+SELECT cust_name
+FROM Customers
+ORDER BY prod_name
+/*
+4. What is wrong with the following SQLstatement? (Try to figure it out without running it.)
+
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state = 'MI'
+ORDER BY cust_name; // issue here
+UNION
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state = 'IL'ORDER BY cust_name;
+
+*/
---
+++ CreatingAdvancedJoins.sql
@@ -0,0 +1,260 @@
+-- =============== Creating Advanced Joins ========= 
+
+-- ===== Using  Table Aliases ===== --
+/*
+In addition to using aliases for column names and calculated fields, SQL also enables you to alias table names
+There are two primary reasons to do this:
+	- To shorten the SQL syntax
+	- To enable multiples uses of the same table within a single SELECT statement.
+*/
+SELECT cust_name, cust_contact
+FROM Customers C, Orders O, OrderItems OI
+WHERE C.cust_id = O.cust_id
+	AND O.order_num = OI.order_num
+	AND OI.prod_id = 'RGAN01';
+
+/*
+You'll notice that the three tables in the FROM clauses all have aliases. Customers
+C establishes C as an alias for Customers, and so on. This approach enables
+you to use the abbreviated C instead of the full text CUstomers. In this example, the
+table aliases were used only in the WHERE clause, but aliases are not limited to just WHERE.
+You can use aliases in the SELECT list, the ORDER BY clause, and in any other part of the statement as well.
+*/
+
+/*
+Caution: No AS in Oracle
+Oracle does not support the AS keyword when aliasing tables. To use aliases in Oracle, simply specify
+the alias without AS (so Customers C instead of CUstomers AS C)
+*/
+
+/*
+It is also worth noting that table aliases are only used during query execution.
+Unlike column aliases, table aliases are never returned to the client.
+*/
+
+-- ==== Using Different Join Types =========== --
+/*
+Thus far you have used only simple joins known as inner joins or equijoins.
+But take a look at three additional join types: the self join, the natural join, and the outer join.
+*/
+
+-- Self Joins
+/*
+One of the primary reasons to use table aliases is to be able to refer to the same table
+more than once in a single SELECT statement.
+
+Suppose you wanted to send a mailing to all the customer contacts who work for the same company
+for which Jim Jones works. This query requires that you first find out which company Jim Jones works for
+and next which customers work for that company. The following is one way to approach this problem:
+*/
+SELECT cust_id, cust_name, cust_contact
+FROM Customers
+WHERE cust_name = (SELECT cust_name
+					FROM Customers
+					WHERE cust_contact = 'Jim Jones');
+
+/*
+cust_id		cust_name	cust_contact
+-------- -------------- --------------
+1000000003	Fun4All		Jim Jones
+1000000004	Fun4All		Denise L. Stephens
+*/
+
+/*
+The first solution uses subqueries. The inner SELECT statement does a simple retrieval to return
+the cust_name of the company that Jim Jones works for. That name is the one used in the WHERE clause of
+the outer query so that all employees who work for that company are retrieved.
+*/
+
+/*
+The same query using a join
+*/
+
+SELECT C1.cust_id, C1.cust_name, C1.cust_contact
+FROM Customers C1, Customers C2
+WHERE C1.cust_name = C2.cust_name
+	AND C2.cust_contact = 'Jim Jones';
+
+/*
+cust_id		cust_name	cust_contact
+------- ----------- --------------
+1000000003	Fun4All		Jim Jones
+1000000004	Fun4All		Denise L. Stephens
+*/
+
+/*
+The two tables needed in this query are actually the same table, and so the Customers table appears
+in the FROM clause twice. Although this is perfectly legal, any references to table Customers would be ambiguous
+because the DBMS does not know which Customers table you are referring to.
+
+To resolve this problem, table aliases are used. The first occurrence of Customers has an alias of C1,
+and the second has an alias of C2. Now those aliases can be used as table names. The SELECT statement,
+for example, uses the C1 prefix to explicitly state the full name of the desired columns. If it did not,
+the DBMS would return an error because there are two of each column named cust_id, cust_name and cust_contact.
+It cannot know which one you want. The WHERE clause first joins the tables and then filters the data by
+cust_contact in the second table to return only the wanted data.
+*/
+
+/*
+Tip: Self Joins Instead of Subqueries
+Self joins are often used to repalce statements using subqueries that retrieve data from the same table as the outer statement.
+Although the end result is the same, manu DBMSs process joins far more quickly than they do subqueries.
+It is usually worth experimenting with both to determine which performs better.
+*/
+
+-- ==== Natural Joins ====== --
+/*
+Whenever tables are joined, at least one column will appear in more than one table (the columns being used
+to create the joins). Standard joins (the inner joins that you learned about) return all data, even multiple
+occurrences of the same column. A natural join simply eliminates those multiple occurrences so that only one
+of each column is returned.
+
+How does it do this? The answer is it doesn't-you do it. A natural join is a join 
+in which you select only columns that are unique. This is typically done using a
+wildcard (SELECT *) for one table and explicit subsets of the columns for all other
+tables. Example:
+*/
+
+SELECT C.*, O.order_num, O.order_date, OI.prod_id, OI.quantity, OI.item_price
+FROM Customers C, Orders O, OrderItems OI
+WHERE C.cust_id = O.cust_id 
+	AND O.order_num = OI.order_num
+	AND prod_id = 'RGAN01';
+
+/*
+A wildcard is used for the first table only. All other columns are 
+explicitly listed so that no duplicate columns are retrieved.
+
+The truth is, every innner join you have created thus far is actually a natural join,
+and you will probably never need an inner join that is not a natural join.
+*/
+
+-- ============= Outer Joins ================ --
+/*
+Most joins relate rows in one table with rows in another. But occasionally, you
+want to include rows that have no related rows. For example, you might use joins to
+accomplish the folowing task:
+	- Count how many orders were placed by each customer, including customers 
+	that have yet to place an order.
+	- List all products with order quantities, including products not ordered by anyone.
+	- Calculate average sale sizes, taking into account customers that have not yet placed an order.
+
+In each of these examples, the join includes table rows that have no associated rows in the related table.
+This type of join is called an outer join.
+*/
+
+/*
+The following SELECT statement is a simple inner join. It retrives a list of all customers and their orders
+*/
+
+SELECT Customers.cust_id, Orders.order_num
+FROM Customers
+	INNER JOIN Orders ON Customers.cust_id = Orders.cust_id
+
+/*
+Outer join syntax is similar. To retrieve a list of all customers including those who have placed no orders
+*/
+SELECT Customers.cust_id, Orders.order_num
+FROM Customers
+	LEFT OUTER JOIN Orders ON Customers.cust_id = Orders.cust_id
+
+/*
+cust_id		order_num
+---------- ---------
+1000000001	20005
+1000000001	20009
+1000000002	NULL
+1000000003	20006
+1000000004	20007
+1000000005	20008
+*/
+
+/*
+This SELECT statement uses the keywords OUTER JOIN to specify the join type 
+(instead of sepcifying it in the WHERE clause). But unlike inner joins, which
+relate rows in both tables, outer joins also include rows with no related rows.
+When using OUTER JOIN syntax, you must use the RIGHT or LEFT keywords to specify
+the table from which to include all rows (RIGHT for the one on the right of OUTER JOIN
+and LEFT for the one on the left). The previous example uses LEFT OUTER JOIN to select 
+all rows from the table on the left in the FROM clause (the Customers table). To select all rows
+from the table on the right, you use a RIGHT OUTER JOIN as seen in this next example:
+*/
+
+SELECT Customers.cust_id, Orders.order_num
+FROM Customers
+	RIGHT OUTER JOIN Orders ON Customers.cust_id = Orders.cust_id;
+
+/*
+Tip: Outer Join Types
+
+Remember that here are always two basic forms of outer joins - the left outer join
+and the right outer join. The only differece between them is the order of tables
+that they are relating. In other words, a left outer join can be turned into a right
+outer join join simply by reversing the other of the tables in the FROM or WHERE clause.
+As such, the two types of outer join can be used interchangeably, and the decision about 
+which one is used is based purely on convenience.
+*/
+
+/*
+There is one other variant of the outer join, one that tends to be rarely used.
+The full outer join retrieves all rows from both tables and relates those that can be related.
+Unlike a left outer join or right outer join, which inclues unrelated rows from a single tables,
+the full ter join inclues unrelated rows from both tables. The syntax for full outer join is follows:
+*/
+
+SELECT Customers.cust_id, Orders.cust_id
+FROM Customers
+	FULL OUTER JOIN Orders ON Customers.cust_id = Orders.cust_id;
+
+/*
+Caution: FULL OUTER JOIN support
+
+The FULL OUTER JOIN syntax is not supported by MariaDB, MySQL, or SQLite.
+*/
+
+-- ========= Using Joins with Aggregate Functions ========== --
+
+/*
+You want to retrieve a list of all customers and the number of orders that each has placed.
+The following code uses the COUNT() function to achieve this:
+*/
+
+SELECT C.cust_id, COUNT(O.order_num) AS num_ord
+FROM Customers C
+	INNER JOIN Orders O ON C.cust_id = O.cust_id
+GROUP BY C.cust_id
+
+/*
+cust_id		num_ord
+---------- --------
+1000000001	2
+1000000003	1
+1000000004	1
+1000000005	1
+*/
+
+/*
+This SELECT statement uses INNER JOIN to relate the Customers and Orders tables to each other.
+The GROUP BY clause groups the data by customer, and so the function call COUNT(O.order_num) counts
+the number of orders for each customer and returns it as num_ord.
+
+Aggregate functions can be used just as easily with other join types. Example:
+*/
+
+SELECT Customers.cust_id,
+		COUNT(Orders.order_num) AS num_ord
+FROM Customers
+	LEFT OUTER JOIN Orders ON Customers.cust_id = Orders.cust_id
+GROUP BY Customers.cust_id;/*cust_id		num_ord
+---------- -------
+1000000001	2
+1000000002	0
+1000000003	1
+1000000004	1
+1000000005	1*//*This example uses a left outer join to include all customers, even those who have not placed any orders.The results show that customer 1000000002 with 0 orders is included this time, unlike when the INNER JOIN was used.*/-- ======== Using Joins and Join Conditions =========== --/*Before I wrap up our two-lesson discussion on joins, I think it is worthwhile to summarize some keypointsregarding joins and their use:	- Pay carefull attention to the type of join being used. More often than not, youll	want an innder join, but there are often valid uses for outer joins too.	- Check your DBMS documentation for the exact join syntax it supports.	- Make sure you use the correct join condition ( regardless of the syntax being used), or you'll	return incorrect data.	- Make sure you always provide a join condition, or you'll end up with the Cartesian product or an error.	- You may include multiple tables in a join and even have different join types for each. 	Although this is legal and often useful, make sure you test each join separately before testing them together.	This will make troubleshooting far simpler.*//*Challenges1. Write a SQL statement using an INNER ORDER to retrieve customer name (cust_name in Customers) and all orders numbers (order_num in Orders) for each.*/SELECT C.cust_name, O.order_numFROM Customers C	INNER JOIN Orders O ON C.cust_id = O.cust_idORDER BY C.cust_nameSELECT C.cust_name, STRING_AGG(O.order_num, ', ') AS order_numbersFROM Customers C	INNER JOIN Orders O ON C.cust_id = O.cust_idGROUP BY C.cust_nameORDER BY C.cust_name/*2. Modify the SQLstatement you just created to list all customers, even those with no orders.*/SELECT C.cust_name, O.order_numFROM Customers C	LEFT OUTER JOIN Orders O ON C.cust_id = O.cust_idORDER BY C.cust_nameSELECT C.cust_name, STRING_AGG(O.order_num, ', ') AS order_numbersFROM Customers C	LEFT OUTER JOIN Orders O ON C.cust_id = O.cust_idGROUP BY C.cust_nameORDER BY C.cust_name/*3. Use an OUTER JOIN to join the Products and OrderItems tables, returning a
+sorted list of product names (prod_name) and the order numbers (order_num)
+associated with each*/SELECT P.prod_name, OI.order_numFROM Products P	LEFT OUTER JOIN OrderItems OI ON P.prod_id = OI.prod_idORDER BY P.prod_name/*4. Modify the SQLstatement created in the previous challenge so that it returns
+a total of number of orders for each item (as opposed to the order numbers).*/SELECT P.prod_name, COUNT(OI.order_num) AS total_numberFROM Products P	LEFT OUTER JOIN OrderItems OI ON P.prod_id = OI.prod_idGROUP BY P.prod_nameORDER BY P.prod_name/*5. Write a SQLstatement to list vendors (vend_id in Vendors) and the number
+of products they have available, including vendors with no products. You’ll want to use an OUTER JOIN and the COUNT() aggregate function to count the
+number of products for each in the Products table. Pay attention: the vend_idcolumn appears in multiple tables, so any time you refer to it, you’ll need to
+fully qualify it.*/SELECT V.vend_id, COUNT(P.prod_id) AS number_of_productsFROM Vendors V	LEFT OUTER JOIN Products P ON V.vend_id = P.vend_idGROUP BY V.vend_idORDER BY number_of_products DESC
