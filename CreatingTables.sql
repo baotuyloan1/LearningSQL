f3ec153bf98e958b80939bb7a3df0f157ab041e0
Nguyen Duc Bao <nguyenduc.bao@avenue-net.com>
2024-10-06 23:18:06 +0700

    Creating Tables

---
+++ CreatingTables.sql
@@ -0,0 +1,14 @@
+-- ============== Creating and Manipulating Tables =================== --
+
+-- ============== Creating Tables ========== --
+
+/*
+SQL is not used just for table data manipulation. Rather, SQL can be used to
+perform all database and table operations, including the creation 
+and manipulation of tables themselves
+
+There are generally two ways to create database table:
+	- Most DBMSs come with an administration tool that you can use to create 
+	and manage database tables interactively 
+*/
+
